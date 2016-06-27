13c275679056c910f480284eea1e4ef7838f8778
Diego Sepulveda Blanco <d.sepulvedabl@gmail.com>
2016-06-27 23:00:28 +0200

    Added year and month selector.

--- diexpenses/ViewModels/MovementsListPageViewModel.cs
+++ diexpenses/ViewModels/MovementsListPageViewModel.cs
@@ -9,17 +9,25 @@
     using System;
     using System.Collections.ObjectModel;
     using System.Diagnostics;
+    using System.Globalization;
     using System.Windows.Input;
     using Views;
     using Windows.UI.Xaml.Navigation;
 
     public class MovementsListPageViewModel : MenuBottomViewModelBase
     {
+        private ObservableCollection<int> years;
+        private ObservableCollection<String> months;
         private ObservableCollection<Movement> items;
+        private int selectedYear;
+        private int selectedMonth;
 
         private static DelegateCommand newMovementCommand;
         private static DelegateCommand deleteMovementCommand;
         private static Base.DelegateCommandWithParameter<Movement> movementSelectedCommand;
+
+        private static DelegateCommand yearChangedCommand;
+        private static DelegateCommand monthChangedCommand;
 
         private IDialogService dialogService;
 
@@ -31,14 +39,43 @@
             deleteMovementCommand = new DelegateCommand(DeleteMovementExecute);
             movementSelectedCommand = new Base.DelegateCommandWithParameter<Movement>(MovementSelectedExecute);
 
+            yearChangedCommand = new DelegateCommand(YearChangedExecute);
+            monthChangedCommand = new DelegateCommand(MonthChangedExecute);
+
+            LoadYears();
+            LoadMonths();
+
+            DateTime today = DateTime.Today;
+            SelectedYear = today.Year;
+            SelectedMonth = today.Month - 1;
             LoadMovements();
+        }
+
+        private void LoadYears()
+        {
+            var today = DateTime.Today;
+            years = new ObservableCollection<int>();
+            for(int i = 2010; i <= today.Year; i++)
+            {
+                years.Add(i);
+            }
+            SelectedYear = today.Year;
+        }
+
+        private void LoadMonths()
+        {
+            months = new ObservableCollection<string>();
+            for (int i = 1; i <= 12; i++)
+            {
+                months.Add(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i));
+            }
         }
 
         private void LoadMovements()
         {
-            DateTime today = DateTime.Today;
-            Debug.WriteLine("Year=" + today.Year + ", Month=" + today.Month);
-            var movementsList = this.DbService.SelectMonthlyMovements(today.Year, today.Month);
+            var month = SelectedMonth + 1;
+            Debug.WriteLine("Year=" + SelectedYear + ", Month=" + month);
+            var movementsList = this.DbService.SelectMonthlyMovements(SelectedYear, month);
             Debug.WriteLine("Number of movements retrieved: " + movementsList.Count);
             Items = new ObservableCollection<Movement>(movementsList);
         }
@@ -56,6 +93,16 @@
         public ICommand MovementSelectedCommand
         {
             get { return movementSelectedCommand; }
+        }
+
+        public ICommand YearChangedCommand
+        {
+            get { return yearChangedCommand; }
+        }
+
+        public ICommand MonthChangedCommand
+        {
+            get { return monthChangedCommand; }
         }
 
         private void NewMovementExecute()
@@ -95,12 +142,64 @@
             NavigationService.NavigateTo(new MovementDetailsPage(movement));
         }
 
+        private void YearChangedExecute()
+        {
+            Debug.WriteLine("YearChangedExecute");
+            LoadMovements();
+        }
+
+        private void MonthChangedExecute()
+        {
+            Debug.WriteLine("MonthChangedExecute");
+            LoadMovements();
+        }
+
+        public ObservableCollection<int> Years
+        {
+            get { return this.years; }
+            set
+            {
+                this.years = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> Months
+        {
+            get { return this.months; }
+            set
+            {
+                this.months = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public ObservableCollection<Movement> Items
         {
             get { return this.items; }
             set
             {
                 this.items = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int SelectedYear
+        {
+            get { return this.selectedYear; }
+            set
+            {
+                this.selectedYear = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int SelectedMonth
+        {
+            get { return this.selectedMonth; }
+            set
+            {
+                this.selectedMonth = value;
                 RaisePropertyChanged();
             }
         }
